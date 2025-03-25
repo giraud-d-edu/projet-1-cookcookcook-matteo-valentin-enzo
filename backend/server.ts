4e32cd86304acb51e6a1e4075b0a61241877405f
Sins-Luxuria <pmatteo032002@gmail.com>
2025-03-25 08:51:38 +0100

    ajout pour link front end

--- backend/server.ts
+++ backend/server.ts
@@ -2,11 +2,12 @@ import { Application } from './deps.ts';
 import Ingredientrouter from './controllers/ingredient.controller.ts';
 import RecetteRouter from './controllers/recette.controller.ts';
 import { connectDB } from './repositories/mongo.ts';
+import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 
 await connectDB();
 
 const app = new Application();
-
+app.use(oakCors({ origin: "http://127.0.0.1:5173" }));
 app.use(Ingredientrouter.routes());
 app.use(Ingredientrouter.allowedMethods());
 app.use(RecetteRouter.routes());
